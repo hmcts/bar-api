5501ceb382f4590b8fea5b1666a9dea785fffd9b
sachi <sachi.kuppuswami@hmcts.net>
2017-09-08 10:36:16 +0100

    Bar-28 - Initial commit

--- api/src/main/java/uk/gov/hmcts/bar/api/controllers/hello/HelloController.java
+++ api/src/main/java/uk/gov/hmcts/bar/api/controllers/hello/HelloController.java
@@ -1,36 +1,41 @@
 package uk.gov.hmcts.bar.api.controllers.hello;
 
 
-import java.util.List;
-import javax.validation.Valid;
-import org.hibernate.validator.constraints.Length;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.bar.api.model.HelloRepository;
 
-
-import static java.util.stream.Collectors.toList;
-
-import static org.springframework.beans.BeanUtils.copyProperties;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import java.util.List;
 
 
 @RestController
 @Validated
 public class HelloController {
 
+    private final HelloDtoMapper helloDtoMapper;
+    private final HelloRepository helloRepository;
 
-
-    @GetMapping("/hello")
-    public String getCategories() {
-        return "Hello Not from DB";
+    @Autowired
+    public HelloController(HelloDtoMapper helloDtoMapper,HelloRepository helloRepository) {
+        this.helloDtoMapper = helloDtoMapper;
+        this.helloRepository = helloRepository;
     }
 
- 
-}
+
+    @ApiOperation(value = "Find  all hellos.",
+        notes = "This endpoint returns all helos .", response = List.class)
+    @GetMapping("/hello")
+    public String getHello() {
+
+        return helloRepository.findByHello("hello").get().getDescription();
+    }
+
+
+
+    }
+
+
+
