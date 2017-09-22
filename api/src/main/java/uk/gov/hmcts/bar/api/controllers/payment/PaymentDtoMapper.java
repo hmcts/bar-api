266d27d61989edfb2885fba7513303963623ab49
sachi <sachi.kuppuswami@hmcts.net>
2017-09-22 14:48:04 +0100

    Refactored package name

---
+++ api/src/main/java/uk/gov/hmcts/bar/api/controllers/payment/PaymentDtoMapper.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.bar.api.controllers.payment;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.bar.api.contract.PaymentDto;
+import uk.gov.hmcts.bar.api.model.Payment;
+
+import java.time.LocalDateTime;
+
+
+@Component
+public class PaymentDtoMapper {
+
+    public PaymentDto toPaymentDto(Payment payment) {
+
+        return PaymentDto.paymentDtoWith()
+            .payeeName(payment.getPayeeName())
+            .caseReference(payment.getCaseReference())
+            .paymentChannel(payment.getPaymentChannel())
+            .paymentDate(String.valueOf(payment.getPaymentDate()))
+            .amount(payment.getAmount())
+            .build();
+    }
+
+
+
+    public Payment toPayment(PaymentDto dto) {
+        return Payment.paymentWith()
+            .payeeName(dto.getPayeeName())
+            .caseReference(dto.getCaseReference())
+            .paymentChannel(dto.getPaymentChannel())
+            .paymentDate(LocalDateTime.parse(dto.getPaymentDate()))
+            .amount(dto.getAmount())
+            .build();
+    }
+
+
+
+}
+
