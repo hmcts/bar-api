cc6a970411549e110a2c70a0cd7e17bdbf422326
Attila Kiss <attila.kiss@hmcts.net>
2018-07-06 10:27:54 +0100

    [BAR-277][AK] Fix failing test

--- api/src/test/java/uk/gov/hmcts/bar/api/controllers/PaymentInstructionStatsTest.java
+++ api/src/test/java/uk/gov/hmcts/bar/api/controllers/PaymentInstructionStatsTest.java
@@ -22,8 +22,8 @@ public class PaymentInstructionStatsTest extends ComponentTestBase {
             .get("/users/1234/payment-instructions/stats?status=TTB")
             .andExpect(status().isOk())
             .andExpect(body().as(Map.class, item -> {
-                assertTrue(item.size() == 1);
-                Map stats = ((ArrayList<Map>)item.get("0")).get(0);
+                assertTrue(item.size() == 2);
+                Map stats = ((ArrayList<Map>)((Map)item.get("content")).get("0")).get(0);
                 assertNull(stats.get("bgc"));
                 assertEquals(1, stats.get("count"));
                 assertEquals("cards", stats.get("payment_type"));
