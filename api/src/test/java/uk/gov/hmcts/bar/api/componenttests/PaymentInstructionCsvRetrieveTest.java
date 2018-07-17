22ed69f8ddb81bc2d2ad1ffbdbf7b12ba991a605
sachi <sachi.kuppuswami@hmcts.net>
2018-07-17 13:35:52 +0100

    added tests

--- api/src/test/java/uk/gov/hmcts/bar/api/componenttests/PaymentInstructionCsvRetrieveTest.java
+++ api/src/test/java/uk/gov/hmcts/bar/api/componenttests/PaymentInstructionCsvRetrieveTest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.bar.api.componenttests;
 
 import org.junit.Assert;
 import org.junit.Test;
+import uk.gov.hmcts.bar.api.data.model.Card;
 import uk.gov.hmcts.bar.api.data.model.CashPaymentInstruction;
 import uk.gov.hmcts.bar.api.data.model.PostalOrder;
 
@@ -11,12 +12,13 @@ import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.bar.api.data.model.Card.cardWith;
 import static uk.gov.hmcts.bar.api.data.model.PostalOrder.postalOrderPaymentInstructionRequestWith;
 
 public class PaymentInstructionCsvRetrieveTest extends ComponentTestBase {
     public static final String SEPARATOR = ",";
     @Test
-    public void givenPostalOrderPaymentInstructionDetails_retrieveAsCvs() throws Exception {
+    public void givenPostalOrderPaymentInstructionDetails_retrieveAsCsv() throws Exception {
         PostalOrder proposedPostalOrderPaymentInstructionRequest = postalOrderPaymentInstructionRequestWith()
             .payerName("Mr Payer Payer")
             .amount(533)
@@ -33,7 +35,7 @@ public class PaymentInstructionCsvRetrieveTest extends ComponentTestBase {
             .payerName("Mr Payer Payer")
             .amount(533)
             .currency("GBP").status("A")
-            .postalOrderNumber("000000").build();
+            .postalOrderNumber("000000").bgcNumber("123456").build();
 
 
         PostalOrder ttbPostalOrderPaymentInstructionRequest = postalOrderPaymentInstructionRequestWith()
@@ -67,12 +69,87 @@ public class PaymentInstructionCsvRetrieveTest extends ComponentTestBase {
         String dailySequenceId = "1";
         String payeeName = "Mr Payer Payer";
         String amount = "5.33";
+        String bgcNumber = "123456";
 
         restActions
             .getCsv("/payment-instructions?startDate=" + paramStartDate)
             .andExpect(status().isOk())
             .andExpect(result -> {
                 String csv  = result.getResponse().getContentAsString();
+                System.out.println(csv);
+                int indexOfdailySequenceId = result.getResponse().getContentAsString().indexOf("1");
+                String actualHeader = csv.substring(0,indexOfdailySequenceId - 2);
+                Assert.assertEquals(expectedHeader,actualHeader);
+                Assert.assertTrue(csv.contains(dailySequenceId));
+                Assert.assertTrue(csv.contains(paymentDate));
+                Assert.assertTrue(csv.contains(payeeName));
+                Assert.assertTrue(csv.contains(amount));
+                Assert.assertTrue(csv.contains(bgcNumber));
+                Assert.assertTrue(csv.contains(recordedUser));
+                Assert.assertTrue(csv.contains(recordedDateTime));
+            });
+    }
+
+
+    @Test
+    public void givenCardPaymentInstructionDetails_retrieveAsCsv() throws Exception {
+        Card proposedCardPaymentInstructionRequest = cardWith()
+            .payerName("Mr Payer Payer")
+            .amount(600)
+            .currency("GBP").status("D")
+            .authorizationCode("000000").build();
+
+        Card validatedCardPaymentInstructionRequest = cardWith()
+            .payerName("Mr Payer Payer")
+            .amount(600)
+            .currency("GBP").status("V")
+            .authorizationCode("000000").build();
+
+        Card approvedCardPaymentInstructionRequest = cardWith()
+            .payerName("Mr Payer Payer")
+            .amount(600)
+            .currency("GBP").status("A")
+            .authorizationCode("000000").build();
+
+
+        Card ttbCardPaymentInstructionRequest = cardWith()
+            .payerName("Mr Payer Payer")
+            .amount(600)
+            .currency("GBP").status("TTB")
+            .authorizationCode("000000").build();
+
+        restActions
+            .post("/postal-orders", proposedCardPaymentInstructionRequest)
+            .andExpect(status().isCreated());
+        restActions
+            .put("/postal-orders/1", validatedCardPaymentInstructionRequest)
+            .andExpect(status().isOk());
+        restActions
+            .put("/postal-orders/1", approvedCardPaymentInstructionRequest)
+            .andExpect(status().isOk());
+        restActions
+            .put("/postal-orders/1", ttbCardPaymentInstructionRequest)
+            .andExpect(status().isOk());
+
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DateTimeFormatter actualFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss");
+        String recordedDateTime = currentDateTime.format(actualFormatter);
+        LocalDate currentDate = LocalDate.now();
+        String paymentDate = currentDate.format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
+        DateTimeFormatter paramFormatter = DateTimeFormatter.ofPattern("ddMMyyyy");
+        String paramStartDate = currentDate.format(paramFormatter);
+        String recordedUser = "1234-fn 1234-ln";
+        String expectedHeader =convertLine(CashPaymentInstruction.CSV_TABLE_HEADER);
+        String dailySequenceId = "1";
+        String payeeName = "Mr Payer Payer";
+        String amount = "6.00";
+
+        restActions
+            .getCsv("/payment-instructions?startDate=" + paramStartDate)
+            .andExpect(status().isOk())
+            .andExpect(result -> {
+                String csv  = result.getResponse().getContentAsString();
+                System.out.println(csv);
                 int indexOfdailySequenceId = result.getResponse().getContentAsString().indexOf("1");
                 String actualHeader = csv.substring(0,indexOfdailySequenceId - 2);
                 Assert.assertEquals(expectedHeader,actualHeader);
@@ -84,6 +161,8 @@ public class PaymentInstructionCsvRetrieveTest extends ComponentTestBase {
                 Assert.assertTrue(csv.contains(recordedDateTime));
             });
     }
+
+
 
     private String convertLine(String[] line){
         return Arrays.stream(line).reduce("", (s, s2) -> s + SEPARATOR + (s2 == null ? "\"\"" : replaceSeparator(s2))).substring(1);
