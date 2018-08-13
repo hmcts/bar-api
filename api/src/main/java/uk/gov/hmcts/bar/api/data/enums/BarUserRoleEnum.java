51b3ac1f856282ffdae9f1228a31ebf9160ddcee
Attila Kiss <attila.kiss@hmcts.net>
2018-08-13 17:38:39 +0100

    [BAR-395][AK] adding more tests

--- api/src/main/java/uk/gov/hmcts/bar/api/data/enums/BarUserRoleEnum.java
+++ api/src/main/java/uk/gov/hmcts/bar/api/data/enums/BarUserRoleEnum.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 public enum BarUserRoleEnum {
     BAR_DELIVERY_MANAGER("bar-delivery-manager"),
-    BAR_SENIOR_CLERK("bar-senior_clerk"),
+    BAR_SENIOR_CLERK("bar-senior-clerk"),
     BAR_FEE_CLERK("bar-fee-clerk"),
     BAR_POST_CLERK("bar-post-clerk"),
     BAR_SUPER_USER("super");
