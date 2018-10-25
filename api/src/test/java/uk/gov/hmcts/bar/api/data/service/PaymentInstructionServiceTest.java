52c6196e90a3d50990742c85893fa364c8a699e5
Attila Kiss <attila.kiss@hmcts.net>
2018-10-25 17:18:35 +0100

    [BAR-483][AK] Validate return payment

--- api/src/test/java/uk/gov/hmcts/bar/api/data/service/PaymentInstructionServiceTest.java
+++ api/src/test/java/uk/gov/hmcts/bar/api/data/service/PaymentInstructionServiceTest.java
@@ -758,4 +758,20 @@ public class PaymentInstructionServiceTest {
         assertEquals(1, count);
     }
 
+    @Test
+    public void shouldReturnThrowExceptionWhenCaseFeeAttached() {
+        when(ff4jMock.check(PaymentActionEnum.RETURN.featureKey())).thenReturn(true);
+        PaymentInstructionUpdateRequest pir = PaymentInstructionUpdateRequest.paymentInstructionUpdateRequestWith()
+            .status("A").action("Return").build();
+        PaymentInstruction existingPaymentInstruction = new ChequePaymentInstruction();
+        existingPaymentInstruction.setCaseFeeDetails(Arrays.asList(new CaseFeeDetail()));
+        when(paymentInstructionRepository.findById(anyInt())).thenReturn(Optional.of(existingPaymentInstruction));
+        try {
+            paymentInstructionService.submitPaymentInstruction(1, pir);
+            fail("should fail here");
+        } catch (PaymentProcessException e) {
+            assertEquals("Please remove all case and fee details before attempting this action.", e.getMessage());
+        }
+    }
+
 }
