1177f5034280dc10ccd1130a211b25b8b6f08f4f
sachi <sachi.kuppuswami@hmcts.net>
2018-11-26 14:35:08 +0000

    BAR-472 updated the comment

--- api/src/main/resources/scripts/clean_payment_instruction_data.sql
+++ api/src/main/resources/scripts/clean_payment_instruction_data.sql
@@ -1,7 +1,7 @@
 /* The following sql statements when executed clears all payment_instruction related data except reference data
  *
  *  1. TRUNCATE TABLE payment_instruction RESTART IDENTITY CASCADE; (clears data in payment_instruction,case_fee_detail,
- *      payment_instruction_status and resets sequences if any in all the tables)
+ *      payment_instruction_status,payment_instruction_payhub_reference and resets sequences if any in all the tables)
  *
  *  2. TRUNCATE TABLE bank_giro_credit CASCADE ; (clears data in bank_giro_credit)
  *
