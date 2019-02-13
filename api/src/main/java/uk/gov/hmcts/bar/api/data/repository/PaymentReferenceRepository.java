247aafceb55fa7f0c20e8dbefeaec17fe5099b15
sachi <sachi.kuppuswami@hmcts.net>
2019-02-13 09:42:08 +0000

    BAR-673 bug fix

--- api/src/main/java/uk/gov/hmcts/bar/api/data/repository/PaymentReferenceRepository.java
+++ api/src/main/java/uk/gov/hmcts/bar/api/data/repository/PaymentReferenceRepository.java
@@ -1,13 +1,19 @@
 package uk.gov.hmcts.bar.api.data.repository;
 
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.bar.api.data.model.PaymentReference;
 
+import javax.persistence.LockModeType;
 import java.util.Optional;
 
 @Repository
 public interface PaymentReferenceRepository extends BaseRepository<PaymentReference, String> {
 
-    Optional<PaymentReference> findById(String siteId);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select pr from PaymentReference pr where pr.siteId = :siteId")
+    Optional<PaymentReference> findOneForUpdate(@Param("siteId") String siteId);
 
 }
