abab78a73399ae7ac653fead47d0e396e6e56f7b
Attila Kiss <attila.kiss@hmcts.net>
2019-03-03 14:54:02 +0000

    [BAR-637][AK] remove caching from user repo, because it's not reliable

--- api/src/main/java/uk/gov/hmcts/bar/api/data/repository/BarUserRepository.java
+++ api/src/main/java/uk/gov/hmcts/bar/api/data/repository/BarUserRepository.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.bar.api.data.repository;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.bar.api.data.model.BarUser;
 
@@ -9,6 +8,5 @@ import java.util.Optional;
 @Repository
 public interface BarUserRepository extends BaseRepository<BarUser, String> {
 
-    @Cacheable(cacheNames = "barusers")
     Optional<BarUser> findBarUserById(String id);
 }
