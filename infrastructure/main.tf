77b25628f133beaa8a7d44744fcf77b343ba52ec
Tim Jacomb <tim.jacomb@hmcts.net>
2020-04-17 15:18:17 +0100

    Terraform fixes (#344)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,26 +1,23 @@
 locals {
-  aseName = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
-  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
-  local_ase = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "core-compute-aat" : "core-compute-saat" : local.aseName}"
+  aseName = "core-compute-${var.env}"
 
-  previewVaultName = "bar-aat"
-  nonPreviewVaultName = "bar-${var.env}"
-  vaultName = "${(var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName}"
+  vaultName = "bar-${var.env}"
   rg_name = "bar-${var.env}-rg"
   asp_name = "bar-asp-${var.env}"
-  vault_rg_name = "${(var.env == "preview" || var.env == "spreview") ? "bar-aat-rg" : local.rg_name}"
+}
 
-
+provider "azurerm" {
+  version = "=1.44.0"
 }
 
 data "azurerm_key_vault" "bar_key_vault" {
   name = "${local.vaultName}"
-  resource_group_name = "${local.vault_rg_name}"
+  resource_group_name = "${local.rg_name}"
 }
 
 data "azurerm_key_vault_secret" "s2s_secret" {
   name      = "bar-S2S-SECRET"
-  vault_uri = "${data.azurerm_key_vault.bar_key_vault.vault_uri}"
+  key_vault_id = "${data.azurerm_key_vault.bar_key_vault.id}"
 }
 
 module "bar-api" {
@@ -43,12 +40,12 @@ module "bar-api" {
     # idam
     IDAM_CLIENT_BASE_URL = "${var.idam_api_url}"
     S2S_SECRET = "${data.azurerm_key_vault_secret.s2s_secret.value}"
-    S2S_AUTH_URL = "http://${var.idam_s2s_url_prefix}-${local.local_env}.service.${local.local_ase}.internal"
+    S2S_AUTH_URL = "http://${var.idam_s2s_url_prefix}-${local.aseName}.service.${var.env}.internal"
     # payhub
     PAYMENT_API_URL = "${var.pay_api_url}"
     # enable/disables liquibase run
     SPRING_LIQUIBASE_ENABLED = "${var.liquibase_enabled}"
-    SITE_API_URL = "http://bar-api-${local.local_env}.service.core-compute-${local.local_env}.internal"
+    SITE_API_URL = "http://bar-api-${local.aseName}.service.core-compute-${var.env}.internal"
   }
 }
 
@@ -69,9 +66,4 @@ resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name      = "${var.component}-POSTGRES-PASS"
   value     = "${module.bar-database.postgresql_password}"
   key_vault_id = "${data.azurerm_key_vault.bar_key_vault.id}"
-}
-
-data "azurerm_key_vault_secret" "appinsights_instrumentation_key" {
-  name = "AppInsightsInstrumentationKey"
-  vault_uri = "${data.azurerm_key_vault.bar_key_vault.vault_uri}"
 }
