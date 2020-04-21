7bc482d04bbd297cd416f438473b23aa7edc452d
Tim Jacomb <tim.jacomb@hmcts.net>
2020-04-21 10:16:45 +0100

    Delete from ASE

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,72 +1,31 @@
-variable "product" {
-  type    = "string"
-  default = "bar"
-}
+variable "product" {}
 
 variable "location" {
-  type    = "string"
   default = "UK South"
 }
 
-variable "component" {
-  type    = "string"
-  default = "bar-api"
+variable "component" {}
 
-}
+variable "env" {}
 
-variable "env" {
-  type = "string"
-}
-variable "subscription" {
-  type = "string"
-}
+variable "subscription" {}
 
 variable "ilbIp"{}
 
 variable "tenant_id" {}
 
 variable "jenkins_AAD_objectId" {
-  type                        = "string"
   description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
-variable "microservice" {
-  type = "string"
-  default = "bar-app"
-}
-
 variable "database_name" {
-  type    = "string"
   default = "bar"
 }
 
 variable "postgresql_user" {
-  type    = "string"
   default = "bar"
 }
 
-variable "idam_api_url" {
-  default = "http://betaDevAccidamAppLB.reform.hmcts.net"
-}
-
-variable "payment_api_url_prefix" {
-  default = "payment-api"
-}
-
-variable "idam_s2s_url_prefix" {
-  default = "rpe-service-auth-provider"
-}
-
-variable "capacity" {
-  default = "1"
-}
 variable "common_tags" {
   type = "map"
-}
-variable "liquibase_enabled" {
-  default = "true"
-}
-variable "pay_api_url" {
-  type = "string"
-  default = "http://payment-api-aat.service.core-compute-aat.internal/"
 }
