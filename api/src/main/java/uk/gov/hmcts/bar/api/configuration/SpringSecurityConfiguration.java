8cb3060fa01f7a0b2a22c2bf46c930763ccb2ce1
amitchidrewar1301 <amit.chidrewar@hmcts.net>
2020-10-15 18:47:12 +0100

    PAY-3638 - Changes in sprint security

--- api/src/main/java/uk/gov/hmcts/bar/api/configuration/SpringSecurityConfiguration.java
+++ api/src/main/java/uk/gov/hmcts/bar/api/configuration/SpringSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import uk.gov.hmcts.reform.auth.checker.spring.useronly.AuthCheckerUserOnlyFilter;
@@ -21,6 +22,22 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
     private AuthCheckerUserOnlyFilter authCheckerFilter;
 
     @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/swagger-ui.html",
+            "/webjars/springfox-swagger-ui/**",
+            "/swagger-resources/**",
+            "/v2/**",
+            "/refdata/**",
+            "/health",
+            "/health/liveness",
+            "/info",
+            "/favicon.ico",
+            "/mock-api/**",
+            "/payment-types",
+            "/");
+    }
+
+    @Override
     @SuppressFBWarnings(value = "SPRING_CSRF_PROTECTION_DISABLED", justification = "It's safe to disable CSRF protection as application is not being hit directly from the browser")
     protected void configure(HttpSecurity http) throws Exception {
 
@@ -29,11 +46,10 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
            .sessionManagement().sessionCreationPolicy(STATELESS).and().anonymous().disable()
            .csrf().disable()
            .formLogin().disable()
-           .logout().disable()
-           .authorizeRequests()
+           .logout().disable();
+           /*authorizeRequests()
            .antMatchers("/swagger-ui.html", "/webjars/springfox-swagger-ui/**", "/swagger-resources/**",
-               "/v2/**", "/health","/health/liveness", "/payment-types", "/info").permitAll()
-           .anyRequest().authenticated();
+               "/v2/**", "/health","/health/liveness", "/payment-types", "/info").permitAll();*/
     }
 
 }
