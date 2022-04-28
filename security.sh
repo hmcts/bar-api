a916a68c6b46d38e4a95cd464bbdf8622608b2f6
“larslunde-hmcts” <lars.lunde@HMCTS.net>
2022-04-28 13:35:31 +0100

    updating to zap-api v3

--- security.sh
+++ security.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
+zap-api-scan.py -t ${TEST_URL}/v3/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL
+cat zap.out
 echo "ZAP has successfully started"
-zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL -J report.json -r api-report.html
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
+zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l High --exit-code False
 mkdir -p functional-output
 chmod a+wx functional-output
-cp api-report.html functional-output/
-
-#Old version (pre fix)
+cp /zap/api-report.html functional-output/
 
 ##!/usr/bin/env bash
 #echo ${TEST_URL}
-#zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL
-#cat zap.out
 #echo "ZAP has successfully started"
-#export LC_ALL=C.UTF-8
-#export LANG=C.UTF-8
-#curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
-#zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
-#zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l High --exit-code False
+#zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL -J report.json -r api-report.html
 #mkdir -p functional-output
 #chmod a+wx functional-output
-#cp /zap/api-report.html functional-output/
-
-
+#cp api-report.html functional-output/
